9f7f597c5a65f59e2fe2631560217a3eb34f1fb7
gaurav065 <warrior.prince652002@gmail.com>
2024-06-28 20:17:04 +0530

    Day 2, big o equations and tutorial how to find them

--- Algorithm analysis, time complexity and space complextiy/tutorial1.py
+++ Algorithm analysis, time complexity and space complextiy/tutorial1.py
@@ -14,12 +14,78 @@ def linear_search(a,x):
         
 
 
+def printer(x):
+    for i in range(x):
+        print("hello")
+
+printer(10)
+
+
+# o(n)---> O(10
+# )
 # [1,2,3,45,5,,6,6,6]
 #  .....>>>
 
+def recurse(n):
+    if(n==0):
+        return
+    else:
+        pass # operation
+    recurse(n-1)
+
+
+#O(N)
+
+for i in range(0,5): #-----> o(n)
+    for j in range(0,5):
+        for k in range(0,5):#-----> o(n)
+            print("*"*k)
+        
+
+#O(n^2)
+
+
+for i in range(0,10, i//2):
+    print(i)
+#o log(n)
+
+for i in range(0,10,i**2):
+    print(i)
+
+
+#o log(logn)
 
 # worst case scenario for this would be O(n)
 # the best case O(1)
 # averag case scenario O(n)
 
 
+#### calculate the time complexity
+
+def func():
+    for i in range(10):
+        print(i)
+
+    for j in range(20):
+        print(j)
+
+
+# when there is a condition where we have 2 loops, the time compexity is calculated as O(i)+O(j), --- O(i+j), i==j O(2i or 2j)== O (n)
+
+
+
+##### find the time complexity of the fucntion
+
+
+def find_max(arr):
+    if not arr:
+        return None
+    
+
+    max_val = arr[0]
+
+    for i in arr[1:]:
+        if i>max_val:
+            max_val=i
+
+    return max_val
